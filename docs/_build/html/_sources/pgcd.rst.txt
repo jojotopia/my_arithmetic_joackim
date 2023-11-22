7bf758565c7e8114da63df9a5eaf2c8e12e24a1c
Joackim PIAUD <joackim.piaud@etudiant.univ-lr.fr>
2023-11-22 18:26:39 +0100

    ajout doc

---
+++ docs/_build/html/_sources/pgcd.rst.txt
@@ -0,0 +1,9 @@
+pgcd
+====
+
+Ce module contient une fonction qui calcule le plus grand commun diviseur (PGCD) de deux nombres entiers.
+
+.. automodule:: my_arithmetic_joackim.pgcd
+    :members:
+    :undoc-members:
+    :show-inheritance:
--- my_arithmetic_joackim/pgcd.py
+++ my_arithmetic_joackim/pgcd.py
@@ -1,4 +1,26 @@
 def pgcd(a, b):
+    """
+    Calcule le plus grand commun diviseur (PGCD) de deux nombres entiers.
+
+    Parameters
+    ----------
+    a : int
+        Le premier nombre entier.
+    b : int
+        Le deuxième nombre entier.
+
+    Returns
+    -------
+    int
+        Le PGCD de a et b.
+
+    Examples
+    --------
+    >>> pgcd(12, 18)
+    6
+    >>> pgcd(7, 13)
+    1
+    """
     if(b == 0):
         return a
     else:
